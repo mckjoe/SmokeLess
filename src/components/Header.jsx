98d7f785ab815f30ee21196c7cb51e829eda8bb8
joe mckinney <mckinney.a.joe@gmail.com>
2018-07-23 16:25:36 -0700

    restyle links and add hover effects to links and buttons.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,14 @@ import nosmoking from './../assets/images/no-smoking.png';
 function Header(){
   const linkStyling = {
     textDecoration: "none",
-    color: "red"
-
   }
+
   return (
     <div className="header">
       <img className="logo" src={nosmoking} />
       <h1>smokeLess</h1>
       <div className="headerLinks">
-        <Link style={linkStyling} to="/details">Details</Link> | <Link style={linkStyling} to="/time">Time</Link> | <Link style={linkStyling} to="/info">Info</Link> | <Link style={linkStyling} to="/health">Health</Link> | <Link style={linkStyling} to="/share">Share</Link>
+        <Link className="links" style={linkStyling} to="/details">Details</Link> | <Link className="links" style={linkStyling} to="/time">Time</Link> | <Link className="links" style={linkStyling} to="/info">Info</Link> | <Link className="links" style={linkStyling} to="/health">Health</Link> | <Link className="links" style={linkStyling} to="/share">Share</Link>
       </div>
     </div>
   );
