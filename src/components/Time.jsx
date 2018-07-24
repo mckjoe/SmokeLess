cd6c14eff9ede281d743a34ac3c0f33c0e533005
joe mckinney <mckinney.a.joe@gmail.com>
2018-07-23 20:50:29 -0700

    change Time.jsx to Class instead of functional component.

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -2,41 +2,46 @@ import React from 'react';
 import TimeAgo from 'react-timeago';
 import PropTypes from 'prop-types';
 
-const time=null;
-
-function convertMS(ms) {
-  var d, h, m, s;
-  s = Math.floor(ms / 1000);
-  m = Math.floor(s / 60);
-  s = s % 60;
-  h = Math.floor(m / 60);
-  m = m % 60;
-  d = Math.floor(h / 24);
-  h = h % 24;
-  return { d: d, h: h, m: m, s: s };
-};
-
-function Time(props){
-  if (props.smokerDetails.length > 0) {
-    var stoppedSmokingDate =  props.smokerDetails[0].date;
+class Time extends React.Component {
+  constructor(props){
+    super(props);
   }
 
-  let quitTime = new Date(stoppedSmokingDate);
-  let todaysDate = new Date();
-  let timeEllapsed = todaysDate - quitTime;
-  var time = convertMS(timeEllapsed);
-  console.log(timeEllapsed);
+   convertMS(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    s = s % 60;
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+    return { d: d, h: h, m: m, s: s };
+  };
 
-  return (
-    <div className='output'>
+  componentDidMount() {}
+
+  render() {
+    if (this.props.smokerDetails.length > 0) {
+      var stoppedSmokingDate =  this.props.smokerDetails[0].date;
+    }
+
+    let quitTime = new Date(stoppedSmokingDate);
+    let todaysDate = new Date();
+    let timeEllapsed = todaysDate - quitTime;
+    var time = this.convertMS(timeEllapsed);
+
+    return (
+      <div className='output'>
       <p>I quit smoking cigarettes</p>
       <p>{time.d} Days</p>
       <p>{time.h} Hours</p>
       <p>{time.m} Minutes</p>
       <p>{time.s} Seconds</p>
       <p>Ago...</p>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 Time.propTypes = {
